b08e19d04d71dbc78a5aa1c75a7180ab75f4ea1c
Harshalgopalani <harshalgopalani@gmail.com>
2025-07-27 19:30:43 +0530

    Add files via upload

---
+++ task2_greeting.py
@@ -0,0 +1,11 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+full_name = first_name + " " + last_name
+
+print("Hello,", full_name, "Welcome to python course!")
+
+#output:
+# Enter your first name: harsh
+# Enter your last name: gopalani
+# Hello, harsh gopalani Welcome to python course!
